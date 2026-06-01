1046b7b9b8034ee90d75e480d23e5137003e7f5b
Antonella Csongedy <csongedyanto@gmail.com>
2026-06-01 02:21:08 -0300

    Realizo implementacion de rutas con React Router.Antonella Csongedy

---
+++ src/views/PerfilUsuario.jsx
@@ -0,0 +1,11 @@
+/// parte provisoria para poder importar
+const PerfilUsuario = () => {
+  return (
+    <section>
+      <h2>Perfil de usuario</h2>
+      <p>########archivo provisorio#########</p>
+    </section>
+  )
+}
+
+export default PerfilUsuario
